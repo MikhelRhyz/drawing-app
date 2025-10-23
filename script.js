8dd028e98992bd37e4a69ca217807ef76997008c
Rhyz Dela Cruz <mikhelrhyz@gmail.com>
2025-10-23 13:57:17 +0800

    Add basic drawing and clear functionality

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 const canvas = document.getElementById('drawCanvas');
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
+const clearBtn = document.querySelector("#clearBtn");
+
+clearBtn.addEventListener("click", () => {
+    ctx.clearRect(0, 0 , canvas.width, canvas.height);
+});
+
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+
+ctx.lineWidth = 4;
+ctx.lineCap = 'round';
+ctx.lineJoin = 'round';
+ctx.strokeStyle = '#000';
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 
-canvas.addEventListener("mousedown", (e) => {
-    isDrawing = true;
-    [lastX, lastY] = [e.offsetX, e.offsetY];
-    console.log(lastX, " " , lastY);
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 });
 
-canvas.addEventListener("mousemove", (e) => {
-    if(!isDrawing) return;
-
-    ctx.beginPath();
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
-
-    [lastX, lastY] = [e.offsetX, e.offsetY];
+canvas.addEventListener('mousemove', (e) => {
+  if (!isDrawing) return;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 });
+
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
